991620454d93db70de1b16bf79de22089fc89224
xiidot1303 <xii1303@inbox.ru>
2025-04-10 11:40:06 +0500

    CORS_ALLOWED_ORIGIN_REGEXES for v0

--- core/settings.py
+++ core/settings.py
@@ -64,6 +64,9 @@ CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = CSRF_TRUSTED_ORIGINS
 
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https:\/\/.*\.lite\.vusercontent\.net$",
+]
 
 ROOT_URLCONF = 'core.urls'
 
