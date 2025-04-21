5da6114ca70b2636f4704f045c04243b0ad81bb9
xiidot1303 <xii1303@inbox.ru>
2025-04-21 17:14:50 +0500

    fix signals

--- app/signals.py
+++ app/signals.py
@@ -10,7 +10,8 @@ from bot.services.notification_service import send_invoice_to_user, send_order_i
 @receiver(post_save, sender=Order)
 def handle_cash_payment_order(sender, instance: Order, created, **kwargs):
     if created and instance.payment_method == "cash":
-        async_to_sync(order_pay)(instance, "cash")
+        instance.payed = True
+        handle_order_payment_status_change(sender, instance)
     elif created:
         # send invoice to user with order information
         send_invoice_to_user.delay(instance.id)
