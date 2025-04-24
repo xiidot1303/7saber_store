2c1ff338833d50d2abd991ca78eaf79126b6e8cf
xiidot1303 <xii1303@inbox.ru>
2025-04-24 10:59:58 +0500

    fix last commit

--- app/scheduled_job/billz_job.py
+++ app/scheduled_job/billz_job.py
@@ -51,8 +51,8 @@ def fetch_products():
                     if p['is_main']:
                         photo = p['photo_url']
                         break
+                size, color = None, None
                 if product_attributes:=product['product_attributes']:
-                    size, color = None, None
                     if product_attributes[0]['attribute_id'] == '6fdcfa5f-42b4-406e-b0fd-712ad011aa5c':
                         size = product_attributes[0]['attribute_value']
                         color = product_attributes[1]['attribute_value']
